8123efb9722faee1c2f3dc3c66234163af0b9b95
gfajardo95 <gfajardo95@gmail.com>
2017-06-28 19:48:30 -0400

    factored out finding the text of rows in a table into a function

--- djangoTDD/functional_test.py
+++ djangoTDD/functional_test.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     # methods that begin with "test_" are treated as tests to be run the by unittest runner ( unittest.main() )
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User goes check out the home page of the to-do app
@@ -39,28 +44,21 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_table('1: Buy peacock feathers')
 
         # There is still a text box inviting the User to add another item
-        # So, the user enters another and sees it update on the screen, adding the item to
-        # the previous list
+        # So, the user enters another and sees it update on the screen
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows])
-
+        # the item is added to the previous list
+        self.check_for_row_in_table('1: Buy peacock feathers')
+        self.check_for_row_in_table('2: Use peacock feathers to make a fly')
+        # A url is generated where the User can go to and see their created list
         self.fail('Finish the test!')
 
-        # A url is generated where the User can go to and see their created list
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
