08f9fa3dc7777f5d6f7f14e46664c71d6baf8551
gfajardo95 <gfajardo95@gmail.com>
2017-06-30 16:29:40 -0400

    new functional test made to see if a url is made for the user's list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
 import time
+import unittest
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
-import unittest  # from the standard library
-
+MAX_WAIT = 2
 
 # Notes about unittest:
 # setup() and tearDown() are special methods that get run before and after every test, so they're a bit like
@@ -17,10 +18,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     # methods that begin with "test_" are treated as tests to be run the by unittest runner ( unittest.main() )
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -47,7 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_table('1: Buy peacock feathers')
+        self.wait_for_row_in_table('1: Buy peacock feathers')
 
         # There is still a text box inviting the User to add another item
         # So, the user enters another and sees it update on the screen
@@ -57,8 +66,42 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # the item is added to the previous list
-        self.check_for_row_in_table('1: Buy peacock feathers')
-        self.check_for_row_in_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_table('1: Buy peacock feathers')
+        self.wait_for_row_in_table('2: Use peacock feathers to make a fly')
 
-        # A url is generated where the User can go to and see their created list
-        self.fail('Finish the test!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        # a unique URL is made when the form does a POST
+        user_url = self.browser.current_url
+        self.assertRegex(user_url, '/lists/.+')
+
+        ## a new user comes in, on a different browser that has
+        ## no browser cookies or such of first user's list
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # new user goes to the site and sees no signs of previous user's to-do list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # new user makes own list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_table('1: Buy milk')
+
+        # new user gets own, unique url
+        new_user_url = self.browser.current_url
+        self.assertRegex(new_user_url, '/lists/.+')
+        self.assertNotEqual(new_user_url, user_url)
+
+        # double check that previous user's data is not on the page, but new user's IS
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
