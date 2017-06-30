62586ab8bde3d567ed7d374171fca1cdf4ddcc8f
gfajardo95 <gfajardo95@gmail.com>
2017-06-30 12:21:39 -0400

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,16 @@
+import time
+
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+
 import unittest  # from the standard library
 
 
 # Notes about unittest:
 # setup() and tearDown() are special methods that get run before and after every test, so they're a bit like
 # the try/except or try/finally
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -22,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
     # methods that begin with "test_" are treated as tests to be run the by unittest runner ( unittest.main() )
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User goes check out the home page of the to-do app
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # User notices the header and title mention To Do Lists
         self.assertIn('To-Do', self.browser.title)
@@ -59,7 +62,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # A url is generated where the User can go to and see their created list
         self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
