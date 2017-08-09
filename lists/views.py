10c30103b21c1b6223193c495753fb48fee9dd83
gfajardo95 <gfajardo95@gmail.com>
2017-08-09 13:57:59 -0400

    create list model, and adjust tests to support the foreign key relation between Item and List

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
-from .models import Item
+from .models import List, Item
 
 # Create your views here.
 
@@ -16,5 +16,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
